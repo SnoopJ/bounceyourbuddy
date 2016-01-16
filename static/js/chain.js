9fdf5536f578b756a34ad53547fa176513fd9a32
SnoopJeDi <snoopjedi@gmail.com>
2016-01-16 17:11:15 -0600

    two goals, switched to P2 physics in anticipation of serverside handling; points, click power, multitouch spawning (for debug), a bunch of things

---
+++ static/js/chain.js
@@ -0,0 +1,327 @@
+var margin = 10;
+//var width = window.innerWidth-margin*2;
+//var height = window.innerHeight-margin*2;
+var height = 360 - margin*2;
+var width = 550 - margin*2;
+
+var CLICKPOWERRATE = 1/10;
+var BOUNCEDELAY = 100;
+var MAXBOUNCES = 8;
+var BALLSPAWNDELAY = 100;
+var BALLKICKDELAY = 250;
+var BALLKICKDURATION = 30;
+var MAXVEL = 1000;
+
+var game = new Phaser.Game(width, height, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+game.focusLoss = function() {};
+
+function render() {
+    balls.map( function(b,i) { 
+        if ( b.collidesWorld() && game.time.now - game.lastBounce > BOUNCEDELAY && Math.abs(b.deltaY) > 1 ) { 
+            game.lastBounce = game.time.now
+            bounceSnd.play() 
+        }
+    })
+    //game.debug.cameraInfo(game.camera, 32, 32);
+}
+function preload() {
+
+    // courtesy of Amon, CC-BY-SA 3.0 ( www.amon.co )
+    // http://opengameart.org/content/football-pitch
+    //game.load.image('pitch', 'assets/misc/pitch.png');
+    game.load.image('pitch', 'assets/misc/clouds.jpg');
+    //game.load.spritesheet('chain', 'assets/sprites/chain.png', 16, 26);
+    //game.load.spritesheet('ball', 'assets/sprites/ball.png', 32, 32);
+
+    // courtesy of alpha_rats, CC-BY 3.0
+    // http://opengameart.org/content/bricks-tiled-texture-64x64
+    game.load.image('ball', 'assets/sprites/SoccerBall.png');
+    // courtesy of gothicfan95, CC0
+    // http://opengameart.org/content/soccer-ball
+    game.load.image('wall', 'assets/sprites/wall.png');
+
+    game.lastballtime = 0;
+    game.camera.bounds = null; // let that camera wander
+
+    // courtesy of paulw2k CC-BY 3.0
+    // https://freesound.org/people/paulw2k/sounds/196461/
+    game.load.audio('goal', 'assets/audio/football.mp3');
+
+    // courtesy of andre.rocha.nascimento CC-BY 3.0
+    // https://freesound.org/people/andre.rocha.nascimento/sounds/51461/
+    game.load.audio('bounce', 'assets/audio/bounce.wav');
+}
+
+var bounces=[];
+function update() {
+
+    game.clickpower = Math.min(100,game.clickpower + CLICKPOWERRATE);
+    var centerx = 0;
+    var centery = 0;
+    for ( var i=0; i < balls.length; i++ ){
+        centerx += balls[i].x
+        centery += balls[i].y
+    }
+    centerx *= 1/(balls.length);
+    centery *= 1/(balls.length);
+    //game.camera.x += 0.09*(centerx-game.camera.position.x);
+    //game.camera.y += 0.09*(centery-game.camera.position.y);
+
+    if (game.fps instanceof Phaser.Text ) {
+        game.fps.text = "FPS: " + game.time.fps;
+    } else {
+        game.fps = game.add.text(0,0,"FPS:");
+    }
+
+    if (game.ptrs instanceof Phaser.Text ) {
+        game.ptrs.text = "Pointers active: " + game.input.countActivePointers();
+    } else {
+        game.ptrs = game.add.text(0,25,"Pointers active:");
+    }
+
+    if (game.score instanceof Phaser.Text ) {
+        game.score.text = "Score: " + game.pts;
+    } else {
+        game.score = game.add.text(0,50,"Score:");
+    }
+
+    game.powergauge.width = game.clickpower / 2;
+    game.powergauge.tint = game.clickpower > 20 ? 0x00ff00 : 0xff0000;
+    //if (game.power instanceof Phaser.Rectangle ) {
+    //    game.power.text = "Click Power: " + Math.floor(game.clickpower);
+    //} else {
+    //    game.power = game.add.text(0,75,"Click Power:");
+    //}
+
+    p = game.input.activePointer;
+    if (p.isDown && p.y < game.height-32) {
+
+        //console.log(sig)
+        var x = p.position.x;
+        var y = p.position.y;
+        if ( game.input.keyboard.isDown( Phaser.KeyCode.ALT ) ) {
+        } else if ( (game.input.keyboard.isDown( Phaser.KeyCode.SHIFT ) || game.input.countActivePointers() > 1 ) && game.time.now-game.lastballtime > BALLSPAWNDELAY ) {
+            game.lastballtime = game.time.now;
+            var vx = 300*(Math.random()-0.5)*2;
+            var vy = 300*(Math.random()-0.5)*2;
+            b = makeABall(x,y,vx,vy);
+        } else {
+            balls.map( function(b,i) { 
+                if ( b.collidesWorld() ) { 
+                    bounceSnd.play() 
+                }
+                if ( game.clickpower > 5 && game.time.now - b.body.lastKick > BALLKICKDELAY  ) {
+                    game.clickpower -= 5;
+                    dv = new Phaser.Point();
+                    dv.x = p.position.x - b.position.x;
+                    dv.y = p.position.y - b.position.y;
+                    dvn = dv.normalize();
+                    Ldv = dv.getMagnitude();
+                    dv = dvn.multiply(200,140);
+                    //dvx = Math.sqrt(Ldv)*50*dvn.x;
+                    //dvy = Math.sqrt(Ldv)*50*dvn.y;
+                    // multitouch -> repelling the balls
+                    //if ( game.input.countActivePointers() > 1 ) {
+                    //    dv.multiply(-1,-1);
+                    //}
+                    //b.body.velocity = b.body.velocity.add(dvx,dvy); 
+                    b.body.kick = dv; 
+                    b.body.lastKick = game.time.now;
+                }
+            })
+        }
+    }
+
+
+    //game.physics.arcade.collide(balls,balls,function ballCollider(b1,b2) {
+    //    if ( game.time.now - game.lastBounce > BOUNCEDELAY && game.bounces < MAXBOUNCES ) {
+    //        game.lastBounce = game.time.now;
+    //        game.bounces++;
+    //        //bounceSnd.play().onStop.add( function() { game.bounces-- } );
+    //    }
+    //});
+    //game.physics.arcade.collide(balls,goal,function goalCollider(b,g) { 
+    //    game.pts++;
+    //    var idx = balls.indexOf(b);
+    //    //console.log("collided ball #" + idx ); 
+    //    if ( !goalSnd.isPlaying ) {
+    //        goalSnd.play();
+    //    }
+    //    b.destroy();
+    //    balls.splice(idx,1);
+    //});
+
+    //for ( var i = 0; i < game.grid.length; i++ ) {
+    //    walls = game.grid[i];
+    //    game.physics.arcade.collide(balls,walls);
+    //}
+
+    //game.physics.arcade.collide(balls,balls,function ballCollider(b1,b2) {
+    //    if ( game.time.now - game.lastBounce > BOUNCEDELAY && game.bounces < MAXBOUNCES ) {
+    //        game.lastBounce = game.time.now;
+    //        game.bounces++;
+    //        //bounceSnd.play().onStop.add( function() { game.bounces-- } );
+    //    }
+    //});
+    for ( var i=0; i < balls.length; i++ ) {
+        b = balls[i];
+        if ( game.time.now-b.body.lastKick < BALLKICKDURATION ) {
+            b.body.velocity.x = b.body.kick.x + b.body.velocity.x;
+            b.body.velocity.y = b.body.kick.y + b.body.velocity.y;
+            b.body.velocity.x = Math.min( Math.max( b.body.velocity.x, -MAXVEL ) ,MAXVEL);
+            b.body.velocity.y = Math.min( Math.max( b.body.velocity.y, -MAXVEL ) ,MAXVEL);
+        } 
+    }
+}
+
+var balls = [];
+var longwall;
+function create() {
+
+    game.clickpower = 100;
+    game.pts = [0,0];
+    game.lastBounce = 0;
+    game.bounces = 0;
+    goalSnd = game.sound.add('goal');
+    bounceSnd = game.sound.add('bounce');
+    //game.sound.setDecodedCallback([ goal ], start, this);
+
+    game.world.setBounds(0,0,game.width,game.height-32);
+
+    var background = game.add.sprite(0, 0, 'pitch' );
+    background.width = game.width;
+    background.height = game.height-32;
+
+    //game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    //game.physics.arcade.gravity.set(0,1200);
+
+    game.time.advancedTiming = true
+
+    //  Length, xAnchor, yAnchor
+    //createRope(40, 400, 64);
+
+    goalR = makeGoal(game.width-50/2,game.height-game.height/3/2-32,50,game.height/3,0,'#ff0000');
+    goalL = makeGoal(50/2,game.height-game.height/3/2-32,50,game.height/3,1,'#0000ff');
+
+    var numballs = 1;
+    for ( var i=0; i < numballs; i++ ) {
+        b = makeABall(game.width*Math.random(),game.height*Math.random(),1000*(Math.random()-0.5)*2,1000*(Math.random()-0.5)*2);
+    }
+    
+    game.grid = Array.apply(null,Array( Math.floor(game.width/32) ))
+    game.grid = game.grid.map( function(x,i) { return Array( Math.floor(game.height/32) ) } )
+
+    //var numwalls = 10;
+    //for ( var i=0; i < numwalls; i++ ) {
+    //    w = toggleWall(Math.random()*game.width, Math.random()*game.height);
+    //}
+
+
+    game.canvas.style.setProperty("position","relative");
+    game.canvas.style.setProperty("left",""+margin+"px");
+    game.canvas.style.setProperty("top",""+margin+"px");
+
+    game.input.onDown.add( function(s) {
+        var x = s.position.x;
+        var y = s.position.y;
+        if( game.input.keyboard.isDown( Phaser.KeyCode.ALT ) && y < game.height-32 ) {
+            toggleWall(x,y);
+        }
+    })
+
+    game.powergauge = (function() {
+        var bmd = game.add.bitmapData(200, 25);
+        bmd.ctx.beginPath();
+        bmd.ctx.rect(0, 0, 200, 25);
+        bmd.ctx.fillStyle = '#ffffff';
+        bmd.ctx.fill();
+        gauge = game.add.sprite(5,75,bmd);
+        return gauge;
+    })();
+}
+
+function toggleWall(x,y) {
+        obj = game.grid[ Math.floor(x/32) ][ Math.floor(y/32) ]
+        if ( obj instanceof Phaser.TileSprite ) { 
+            obj.destroy()
+            game.grid[ Math.floor(x/32) ][ Math.floor(y/32) ] = undefined;
+        } else {
+            var wall = game.add.tileSprite(Math.floor(x/32)*32, Math.floor(y/32)*32, 32, 32, 'wall');
+            game.physics.enable( wall, Phaser.Physics.ARCADE )
+            wall.body.immovable = true;
+            game.grid[ Math.floor(x/32) ][ Math.floor(y/32) ] = wall;
+            walls.push(wall)
+        }
+}
+
+function makeABall(x,y,vx,vy) {
+    var ball = game.add.sprite(x, y, 'ball', 0);
+    ball.bringToTop();
+
+    ball.tint = Math.floor( 0xFFFFFF * Math.random() ) + 0x777777;
+    
+    ball.width = 16
+    ball.height = 16
+
+    // ARCADE PHYSICS
+    //game.physics.enable(ball, Phaser.Physics.ARCADE);
+    //ball.body.collideWorldBounds = true;
+    //ball.body.gravity.set(0,800);
+    //ball.body.bounce.setTo(1.0,0.75);
+    //ball.body.drag.setTo(0.9,0.3);
+    // ball.body.velocity.setTo(vx,vy);
+
+    // P2 PHYSICS
+    game.physics.p2.enable(ball, false);
+    game.physics.p2.gravity.y = 800;
+    game.physics.p2.restitution = 1;
+    ball.body.velocity.x = vx;
+    ball.body.velocity.y = vy;
+    ball.body.onEndContact.add( function() { 
+        if ( game.time.now - game.lastBounce > BOUNCEDELAY && Math.abs(this.body.velocity.y) > 1 ) {
+            game.lastBounce = game.time.now;
+            bounceSnd.play(); 
+        }
+    },ball)
+
+    ball.body.lastKick = 0;
+
+    ball.collidesWorld = function() {
+        return false;
+        //return ( this.body.blocked.up || this.body.blocked.down || this.body.blocked.left || this.body.blocked.right )
+    }
+
+    balls.push( ball );
+    return ball;
+}
+
+
+function makeGoal(x,y,width,height,idx,color) {
+    var goal;
+    var bmd = game.add.bitmapData(width, height);
+     
+    bmd.ctx.beginPath();
+    bmd.ctx.rect(0, 0, width, height);
+    bmd.ctx.fillStyle = color;
+    bmd.ctx.fill();
+    goal = game.add.sprite(x,y, bmd);
+    goal.anchor.setTo(0.5, 0.5);
+    //game.physics.enable( goal, Phaser.Physics.ARCADE )
+    game.physics.p2.enable( goal, false )
+    //goal.body.immovable = true;
+    goal.body.static = true;
+    goal.body.data.shapes[0].sensor = true
+    goal.body.onBeginContact.add( function(s) { 
+        ball = s.sprite;
+        balls.splice( balls.indexOf(s.sprite), 1 )
+        ball.body.onEndContact.removeAll();
+        ball.destroy();
+        if ( !goalSnd.isPlaying ) {
+            goalSnd.play();
+        }
+        game.pts[idx]++;    
+        //console.log( balls.indexOf(s.sprite) ) 
+    })
+    return goal;
+}
