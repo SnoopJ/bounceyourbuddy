8214ed3e3fc9c2b297df002a8f35740b4983de78
SnoopJeDi <snoopjedi@gmail.com>
2016-01-18 18:13:39 -0600

    didn't commit much near the end of the hackathon...cleanup commit to get things proper

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 var p2 = require('p2'); 
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
-
 var http = require('http')
-    //fs = require('fs'),
-    //// NEVER use a Sync function except at start-up!
-    //index = fs.readFileSync(__dirname + '/static/index.html');
 
 var serve = serveStatic('./static');
 var app = http.createServer(function(req,res) {
@@ -37,7 +33,6 @@ world = new p2.World({
 var resetWorld = function () {
     io.emit( 'reset' );
     clearInterval(physicsLoop);
-    //world.clear();
     world.bodies.map( function(b,i) {
         world.removeBody(b);
     });
@@ -46,14 +41,12 @@ var resetWorld = function () {
     console.log("run new world");
     setTimeout( function() {
         b = spawnBall( (530-300)/20,100/20, -5, 0, curid );
-        //console.log(b.ballid);
         io.emit('spawnBall', { x: (530-300), y: 100, vx: -5/20, vy: 0, id:curid });
         curid++;
         var timeStep = 1/40;
         physicsLoop = setInterval( function() { 
             world.step(timeStep); 
             sendPhysicsUpdate(); 
-            //console.log(ball.position);
         }, 1000*timeStep );
     }, 3000);
 }
@@ -76,7 +69,6 @@ var setUpWorld = function () {
             position:[x,y],
             velocity: [vx, vy],
             angularVelocity: 0,
-            //position:[(530-300)/20,300/20]
         });
         ball.addShape( new p2.Box({ width: 16/20, height: 16/20, material: ballMaterial }) );
         ball.ballid = id;
@@ -123,9 +115,6 @@ var setUpWorld = function () {
             console.log(clients);
             io.emit('goal', { scoredon: scoredon, ballid: evt.bodyB.ballid } );
             resetWorld();
-            //balls.splice( balls.indexOf(evt.bodyB), 1 );
-            //world.removeBody(evt.bodyB);
-            //delete(evt.bodyB);
         } if ( evt.bodyB === goalL || evt.bodyB === goalR ) {
             console.log("GOOOOOAAAAAAAAAAAAALLLL"); 
             scoredon = "nobody?!";
@@ -134,12 +123,7 @@ var setUpWorld = function () {
             console.log( "Against " + scoredon ); 
             io.emit('goal', { scoredon: scoredon, ballid: evt.bodyA.ballid } );
             resetWorld();
-            //balls.splice( balls.indexOf(evt.bodyA), 1 );
-            //world.removeBody(evt.bodyA);
-            //delete(evt.bodyA);
         }
-        //console.log(evt.bodyA);
-        //console.log(evt.bodyB);
     })
 
     var groundShape = new p2.Plane( { material: worldMaterial } );
@@ -156,7 +140,6 @@ var setUpWorld = function () {
         type: p2.Body.STATIC,
         position:[0,300/20]
     });
-    //ceilingShape.collisionResponse = false;
     ceiling.addShape(ceilingShape);
     world.addBody(ceiling);
 
@@ -241,7 +224,6 @@ io.on('connection', function(socket){
             physicsLoop = setInterval( function() { 
                 world.step(timeStep); 
                 sendPhysicsUpdate(); 
-                //console.log(ball.position);
             }, 1000*timeStep );
         } else { // two people already playing
             console.log("Sorry, buddy, we're full...")
